724ef52b30c97cc36b7c50ab8459392bb8426da5
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-09-01 14:43:29 +0100

    RestTemplate Configuration

--- src/main/java/com/example/resttemplateproject/Config/RestTemplateConfig.java
+++ src/main/java/com/example/resttemplateproject/Config/RestTemplateConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+
 @Component
 public class RestTemplateConfig {
     @Bean
